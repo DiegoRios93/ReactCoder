7e69b9bee2105361d7a9c00c21e12fecfe17df91
Diego Rios <diegorios.9335@gmail.com>
2021-12-22 20:55:06 -0300

    Primer commit- clase 2 React

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Diego Rios - Clase 2 Coder
         </a>
       </header>
     </div>
