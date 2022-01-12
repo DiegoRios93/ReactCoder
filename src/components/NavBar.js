77b32282fc023e21c8ff3f85f3f55483ef745b30
Diego Rios <diegorios.9335@gmail.com>
2022-01-12 18:52:50 -0300

    Primer menu con react y css puro

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css';
+
+function NavBar () {
+	return <nav>
+		<div>
+			<h1>PadelShop!</h1>
+		</div>
+		<div className="lista">
+			<ul>
+				<li><a href="!#">PRODUCTOS</a></li>
+				<li><a href="!#">MÁS VENDIDOS</a></li>
+				<li><a href="!#">CARRITO</a></li>
+				<li><a href="!#">CONTACTO</a></li>
+			</ul>
+		</div>
+		
+	</nav>
+}
+
+export default NavBar;
